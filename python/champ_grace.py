5243683ce6fab38b480db5bb536fc2c80a4aecbf
Dongjie Guo <502097523@qq.com>
2016-10-14 16:06:21 -0400

    Delete unused functions from champ_grace.py
    Deleted functions: add_index, add_imf, add_epoch_sb, add_epoch_time,
    	relative_density_to_previous_orbit_lt,
    	relative_density_to_previous_orbit_mlt,
    	get_lon_lat_density, get_lt_lat_density,
    	data_gap_nan,
    	add_arglat

--- python/champ_grace.py
+++ python/champ_grace.py
@@ -1,7 +1,6 @@
 #--------------------------------------------------------------------------------
-# v 1.0
 #
-# By Dongjie, USTC, on Fri Sep 16 23:23:39 CST 2016
+# By Dongjie, USTC/UM, on Fri Sep 16 23:23:39 CST 2016
 #
 # Class for the CHAMP and GRACE 3-degree densities and winds.
 # Also include functions to read data from file
@@ -24,9 +23,6 @@
 #
 #           satellite_position_lt_lat: show the satellite location in LT-LAT
 #                   coordinates
-#
-#           data_gap_nan: Add nan to data gap, used for plot. There is a data gap
-#                   when the time interval is greater than 2 hours
 #
 #           contourf_date_lat: Contourf of rho, rho400... as a function of date and
 #                   lat
@@ -203,17 +199,6 @@ class ChampDensity(pd.DataFrame):
             self = self[(self.isup) | (self.isdown)]
             return ChampDensity(self)
 
-    def add_arglat(self):
-        # This algorithm is wrong. But let it be.
-        # Argument of latitude is the angle from the ascending node
-        # to the satellite position along the orbital path.
-        if not 'isup' in self:
-            self = self.add_updown()
-        self['arglat'] = self.lat3.copy()
-        self.loc[self.isup,'arglat'] = (360+self.loc[self.isup,'arglat'])%360
-        self.loc[self.isdown,'arglat'] = 180-self.loc[self.isdown,'arglat']
-        return ChampDensity(self)
-
     def orbit_mean(self,lats=(-85,85),updown='up'):
         """ Get the orbit mean density during specified latitude range and
         specified ascending or descending orbit
@@ -279,18 +264,6 @@ class ChampDensity(pd.DataFrame):
         return hc
 
 
-    def data_gap_nan(self):
-        """ If there are data gaps,insert nan, for plt.plot
-        """
-        tmp = self.index
-        tmp1 = (self.index-pd.Timestamp('2000-1-1'))/pd.Timedelta('1H')
-        tmp2 = np.insert(np.diff(tmp1),0,0)
-        tmp3 = np.argwhere(tmp2>2)
-        tmp3 = tmp3.reshape((tmp3.size,))
-        tmp = np.insert(tmp,tmp3,tmp[tmp3-1]+pd.Timedelta('0.5H'))
-        return self.reindex(tmp)
-
-
     def contourf_date_lat(self, ax, whichcolumn='rho400',
                           updown='up', **kwargs):
         """ A contourf of multiple-day density versus date and latitude.
@@ -345,260 +318,6 @@ class ChampDensity(pd.DataFrame):
                           .format(tmp.index[0].year),fontsize=14)
             ax.set_ylabel('Latitude', fontsize=14)
             return hc#, rho
-
-
-    def get_lt_lat_density(self, whichdensity='rho400', latbin=10,
-                           ltbin=8, interpolate=False, latn=180, ltn=48):
-        """ Get lt, lat, data for polar contourf. This function is for a large
-        data set that covers all the lt and lat ranges.
-
-        Args:
-            whichdensity: rho, rho400, rho410...
-            latbin: lat window, harmonic of 90: 3,10,30....
-            ltbin: local time window, harmonic of 24: 1,2,3,4,6,8...
-            interpolate: bool, whether to interpolate data to higher resolution
-            latn, ltn: for interpolation, bin numbers of lat and lt, integral
-                multiple of 180 and 24 respectively
-        Returns:
-            lt, lat, density. All are 2D array
-
-        Note: the sample number in each lat*lt grid is not considered
-              maybe the interpolation is unnecessary
-        """
-        df = self[['lat', 'LT', whichdensity]]
-        df['lat'] = pd.cut(df['lat'], bins=np.arange(-90,90+1e-10,latbin),
-                     labels=np.arange(-90,90+1e-10,latbin)[:-1]+latbin/2,
-                     include_lowest=True)
-        df['LT'] = pd.cut(df['LT'], bins=np.arange(0,24+1e-10,ltbin),
-                     labels=np.arange(0,24+1e-10,ltbin)[:-1]+ltbin/2,
-                     include_lowest=True)
-        df = df.groupby(['lat', 'LT']).mean().reset_index().pivot(
-            'lat', 'LT', whichdensity)
-        df[df.columns[0]+24] = df[df.columns[0]] # data is periodic in lt
-        if interpolate:
-            lt = np.linspace(df.columns.min(), df.columns.max(), ltn)
-            lat = np.linspace(-90, 90, latn)
-            lt, lat = np.meshgrid(lt, lat)
-            df = df.unstack()
-            df.name = whichdensity
-            df = df.reset_index()
-            data = griddata((df['lat'], df['LT']/12*180),
-                            df[ whichdensity], (lat, lt/12*180), method='cubic')
-            return lt, lat, data
-        else:
-            lt, lat = np.meshgrid(df.columns, df.index)
-            return lt, lat, df.values
-
-
-    def get_lon_lat_density(self, whichdensity='rho400', latbin=10,
-                            lonbin=45, interpolate=False,
-                            latn=180, lonn=360):
-        """ return lon, lat, density for polar contourf. This function is for a
-        large data set that covers all the lon and lat ranges.
-
-        Args:
-            whichdensity: rho, rho400, rho410...
-            latbin: lat window, harmonic of 90: 3,10...
-            lonbin: lon window, harmonic of 360: 10,30...
-            interpolate: bool, whether to interpolate data to higher resolution
-            latn, lonn: for interpolation, bin numbers of  lat and lon,
-                intergral multiple of 180 and 360, respectively.
-        Returns:
-            lon, lat, density
-
-        Note: the sample number in each lat*lon grid is not considered
-              maybe the interpolation is unnecessary
-        """
-        df = self[['lat', 'lon', whichdensity]]
-        df['lat'] = pd.cut(
-                df['lat'], bins=np.arange(-90,90+1e-10,latbin),
-                labels=np.arange(-90,90+1e-10,latbin)[:-1]+latbin/2,
-                include_lowest=True)
-        df['lon'] = pd.cut(
-                df['lon'], bins=np.arange(-180,180+1e-10,lonbin),
-                labels=np.arange(-180,180+1e-10,lonbin)[:-1]+lonbin/2,
-                include_lowest=True)
-        df = df.groupby(['lat', 'lon']).mean().reset_index().pivot(
-            'lat', 'lon', whichdensity)
-        df[df.columns[0]+360] = df[df.columns[0]] # data is periodic in lon
-        if interpolate:
-            lon = np.linspace(df.columns.min(), df.columns.max(), lonn)
-            lat = np.linspace(-90, 90, latn)
-            lon, lat = np.meshgrid(lon, lat)
-            df = df.unstack()
-            df.name = whichdensity
-            df = df.reset_index()
-            data = griddata((df['lat'], df['lon']),
-                            df[ whichdensity], (lat, lon), method='cubic')
-            return lon, lat, data
-        else:
-            lon, lat = np.meshgrid(df.columns, df.index)
-            return lon, lat, df.values
-
-
-    def relative_density_to_previous_orbit_lt(self, whichcolumn='rho400'):
-        """Get the time interval (dhour_po) between two continuous orbits and
-        density relative to in the previous orbit (rrho_po).
-        (rho-rho_po)/(rho_po*dhour), unit: /h
-
-        Input:
-            whichcolumn: 'rho', 'rho400', 'rho410' ...
-
-        Output:
-            self with columns 'dhour_po', 'drho_po',
-            'rrho_po' and 'distance_lt' in LT-lat coordinates added
-
-        Note:
-            The pole values may be unreliable
-        """
-        self = self.add_updown()
-        groupdensity = self.groupby(['isup','lat3'])
-        def rtl(x):
-            x['dhour_po'] = np.insert(np.diff(x.index),0,'nat')/np.timedelta64(1,'h')
-            x['rrho_po'] = x[whichcolumn].diff()/np.roll(x[whichcolumn],1)/x['dhour_po']
-            x['drho_po'] = x[whichcolumn].diff()/x['dhour_po']
-
-            lat = x['lat']/180*np.pi
-            latpo = np.roll(lat, 1)
-            deltaLT = x['LT'].diff()/12*np.pi
-            x['distance_lt'] = 6378*np.arccos(
-                    np.sin(lat)*np.sin(latpo) +
-                    np.cos(lat)*np.cos(latpo)*np.cos(deltaLT))
-            return x
-        self = groupdensity.apply(rtl)
-        fp1 = (self.dhour_po>1.6)
-        fp2 = (self.dhour_po<1.45)
-        fp3 = (self.distance_lt>600)
-        fp = fp1 | fp2 | fp3
-        self.loc[fp,'dhour_po'] = np.nan
-        self.loc[fp,'rrho_po'] = np.nan
-        self.loc[fp,'drho_po'] = np.nan
-        self.loc[fp,'distance_lt'] = np.nan
-        return ChampDensity(self)
-
-
-    def relative_density_to_previous_orbit_mlt(self, whichcolumn='rho400'):
-        """Relative density between 2 adjecent orbits.
-
-        Input:
-            whichcolumn: 'rho', 'rho400', 'rho410' ...
-
-        Output:
-            self with columns 'dhour_po', 'distance_mlt', 'rrho_po' and 'dpoints' added
-
-        Note:
-            The pole values may be unreliable
-        """
-        Mlat = np.array(self.Mlat)/180*np.pi
-        Mlt = np.array(self.MLT)/12*np.pi
-        time = (self.index-pd.Timestamp('2000-1-1'))/pd.Timedelta('1h')
-        rho = np.array(self[whichcolumn])
-
-        dib, dl = 110, 16
-        dd = np.ones([len(Mlat),dl])*np.nan
-        dh = np.ones([len(Mlat),dl])*np.nan
-        dr = np.ones([len(Mlat),dl])*np.nan
-        for di in np.arange(dib,dib+dl):
-            Mlatpo, Mltpo = np.roll(Mlat,di), np.roll(Mlt,di)
-            timepo, rhopo = np.roll(time,di), np.roll(rho,di)
-            dd[di:,di-dib] = 6378*np.arccos(
-                    np.sin(Mlat[di:])*np.sin(Mlatpo[di:]) +
-                    np.cos(Mlat[di:])*np.cos(Mlatpo[di:])*np.cos(Mlt[di:]-Mltpo[di:]))
-            dh[di:,di-dib] = time[di:]-timepo[di:]
-            dr[di:,di-dib] = (rho[di:]-rhopo[di:])/rhopo[di:]/dh[di:,di-dib]
-        ddmin = np.argmin(dd,axis=1)
-
-        ddout = dd[np.arange(len(Mlat)),ddmin]
-        dhout = dh[np.arange(len(Mlat)),ddmin]
-        drout = dr[np.arange(len(Mlat)),ddmin]
-        dpout = ddmin + dib
-        self['dhour_po'], self['distance_mlt'], self['rrho_po'], self['dpoints'] = (
-                dhout, ddout, drout, dpout)
-        fp1 = self.distance_mlt>800
-        fp2 = (self.dhour_po>2) | (self.dhour_po<1)
-        fp = fp1 | fp2
-        self.loc[fp,['dhour_po', 'distance_mlt', 'rrho_po', 'dpoints']] = np.nan
-        return ChampDensity(self)
-
-
-    def add_index(self):
-        """Add solar or geomagnetic parameters to ChampDensity.
-
-        Output:
-            self with columns 'ap', 'f107'... added
-        """
-        dates = pd.date_range(self.index.min().date(),
-                              self.index.max().date(),
-                              freq='1d')
-        index = get_index(dates)
-        index = index.reindex(self.index, method='ffill')
-        self = pd.concat([self, index], axis=1)
-        return ChampDensity(self)
-
-
-    def add_imf(self):
-        """Add IMF components to ChampDensity.
-
-        Output:
-            self with columns 'Bx', 'Bye'... added
-        """
-        dates = pd.date_range(self.index.min().date(),
-                              self.index.max().date(),
-                              freq='1d')
-        imf = get_imf(dates)
-        imf = imf.reindex(self.index, method='ffill')
-        self = pd.concat([self, imf], axis=1)
-        return ChampDensity(self)
-
-
-    def add_epoch_sb(self, exclude_cir=True):
-        """Add the epoch days relative to the solar wind sector polarity
-        reversing dates.
-
-        Output:
-            self with columns 'epochat', 'epochta' added
-        """
-        sblist = get_sblist()
-        sblist = sblist[self.index.min().date():self.index.max().date()]
-        if exclude_cir:
-            cirlist = get_cirlist()
-            sblist = sblist[cirlist.min().date():
-                            cirlist.max().date()]
-            #cirlist = (
-            #        cirlist.
-            #        append([cirlist+pd.Timedelta('1D'),
-            #                cirlist+pd.Timedelta('-1D')]).unique())
-            sblist = sblist[~sblist.index.isin(pd.to_datetime(cirlist.date))]
-        groupsb = sblist.groupby('sbtype')
-        for sbat in groupsb.get_group('away-toward').itertuples():
-            ldate = sbat[0]-pd.Timedelta(sbat[2],'d')
-            rdate = sbat[0]+pd.Timedelta(sbat[3],'d')
-            self.loc[ldate:rdate,'epochat'] = (
-                    self[ldate:rdate].index - sbat[0])/np.timedelta64(1,'D')
-        for sbta in groupsb.get_group('toward-away').itertuples():
-            ldate = sbta[0]-pd.Timedelta(sbta[2],'d')
-            rdate = sbta[0]+pd.Timedelta(sbta[3],'d')
-            self.loc[ldate:rdate,'epochta'] = (
-                    self[ldate:rdate].index - sbta[0])/np.timedelta64(1,'D')
-        return ChampDensity(self)
-
-
-    def add_epochtime(self, epoch0, lday=5, rday=5):
-        """ add epoch times to self
-        *** be careful of the overlap ****
-
-        Input:
-            epoch0: pd.DatatimeIndex, zero epoch of special cases
-            lday & rday: the maximum days before and after 0 epoch time
-        Output:
-            self with column epochtime (unit: day)  added
-        """
-        for k in epoch0:
-            maxlepoch = k - pd.Timedelta(lday,'d')
-            maxrepoch = k + pd.Timedelta(rday,'d')
-            self.loc[maxlepoch:maxrepoch, 'epochtime'] = (
-                    self[maxlepoch:maxrepoch].index - k)/np.timedelta64(1,'D')
-        return ChampDensity(self)
 
 
 class ChampWind(ChampDensity):
