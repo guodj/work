32a36d7eb8181bf2ad3a4adef98d7fe15f79ce5f
Dongjie Guo <502097523@qq.com>
2016-11-03 16:51:43 -0400

    remove func2() from work3_100.py
    func2 is a function that calculates average densities before and after
    IMF By change.

--- python/work3_100.py
+++ python/work3_100.py
@@ -71,100 +71,14 @@ def func1():
         input()
     return
 
-def func2():
-    #----------------------------------------
-    # Case 1
-    #    bdate = '2010-5-29 03:00:00'
-    #    mdate = '2010-5-29 12:00:00'
-    #    edate = '2010-5-29 21:00:00'
-    # Case 2
-    #    bdate = '2009-11-14 06:00:00'
-    #    mdate = '2009-11-14 11:00:00'
-    #    edate = '2009-11-14 18:00:00'
-    # Case 3
-    #    bdate = '2009-12-12 15:00:00'
-    #    mdate = '2009-12-12 20:00:00'
-    #    edate = '2009-12-13 06:00:00'
-    # Case 4
-    #    bdate = '2010-01-02 02:00:00'
-    #    mdate = '2010-01-02 07:00:00'
-    #    edate = '2010-01-02 12:00:00'
-    # Case 5
-    #    bdate = '2010-01-03 03:00:00'
-    #    mdate = '2010-01-03 12:00:00'
-    #    edate = '2010-01-03 16:00:00'
-    # Case 6
-    #    bdate = '2010-01-05 09:00:00'
-    #    mdate = '2010-01-05 18:00:00'
-    #    edate = '2010-01-06 03:00:00'
-    # Case 7
-    #    bdate = '2010-04-12 09:00:00'
-    #    mdate = '2010-04-12 18:00:00'
-    #    edate = '2010-04-13 00:00:00'
-    # Case 8
-    bdate = '2010-05-18 21:00:00'
-    mdate = '2010-05-19 06:00:00'
-    edate = '2010-05-19 23:00:00'
-    #----------------------------------------
-    dench = ChampDensity(bdate,edate,satellite='champ')
-    dench['arglat'] = mf.lat2arglat(dench.lat)
-    mdench = dench.groupby(
-            [np.floor(dench.arglat/3)*3, dench.index<mdate])['rho'].mean()
-    dengr = ChampDensity(bdate,edate,satellite='grace')
-    dengr['arglat'] = mf.lat2arglat(dengr.lat)
-    mdengr = dengr.groupby(
-            [np.floor(dengr.arglat/3)*3, dengr.index<mdate])['rho'].mean()
-    dengo = GoceData(bdate,edate)
-    dengo['arglat'] = mf.lat2arglat(dengo.lat)
-    mdengo = dengo.groupby(
-            [np.floor(dengo.arglat/3)*3, dengo.index<mdate])['rho'].mean()
-    den = (dengo, dench, dengr)
-    mden = (mdengo, mdench, mdengr)
-    fig, ax = plt.subplots(3,2,sharex='col', sharey='row', figsize=(7.4,7.9))
-    blat = 60
-    xl = ((blat, 180-blat), (180+blat, 360-blat))
-    yl = ((5, 35), (0, 20), (0, 0.3))
-    ylb = ('GOCE, $10^{-12} kg/m^{-3}$',
-           'CHAMP, $10^{-12} kg/m^{-3}$',
-           'GRACE, $10^{-12} kg/m^{-3}$')
-    tl = ('North', 'South')
-    for k0 in range(3):
-        for k1 in range(2):
-            plt.sca(ax[k0,k1])
-            tmp1, tmp2 = den[k0], mden[k0]
-            plt.plot(tmp1[bdate:mdate].arglat, tmp1[bdate:mdate].rho/1e-12,
-                     'o', color='lightcoral', alpha=1, markersize=3)
-            plt.plot(tmp1[mdate:edate].arglat, tmp1[mdate:edate].rho/1e-12,
-                     'o', color='lightblue', alpha=1, markersize=3)
-            line1, = plt.plot(
-                    tmp2[:,False].index, tmp2[:,False]/1e-12, 'b', label='After'
-                    )
-            line2, = plt.plot(
-                    tmp2[:,True].index, tmp2[:,True]/1e-12, 'r', label='Before')
-            plt.xticks(np.arange(0,361,10))
-            plt.xlim(xl[k1])
-            plt.ylim(yl[k0])
-            plt.gca().set_frame_on(True)
-            if k0 == 0:
-                plt.title(tl[k1])
-            if k1 == 0:
-                plt.ylabel(ylb[k0])
-            if (k0 == 2) & (k1 == 1):
-                plt.legend(handles=(line2, line1))
-            if k0 == 2:
-                plt.xlabel('Argument of Latitude')
-            plt.grid('on')
-    plt.tight_layout()
-    return
-
 def func3():
     '''
     Case analysis: time constant of density response to IMF By
     '''
     rtime = '2010-03-22 12:00:00'
     rtime = pd.Timestamp(rtime)
-    btime = rtime - pd.Timedelta('12 h')
-    etime = rtime + pd.Timedelta('12 h')
+    btime = rtime - pd.Timedelta('1.5 h')
+    etime = rtime + pd.Timedelta('1.5 h')
     # IMF and Kan-Lee electric field variations
     from matplotlib.ticker import AutoMinorLocator
     # By, Bz, K-L electric field
@@ -181,7 +95,7 @@ def func3():
         plt.sca(ax1[k0])
         tmp = omni[pv[k0]]
         plt.plot((tmp.index-rtime)/pd.Timedelta('1h'), tmp, 'k')
-        plt.xticks(np.arange(-30, 31, 3))
+        plt.xticks(np.arange(-30, 31, 1))
         plt.xlim((btime-rtime)/pd.Timedelta('1h'),
                  (etime-rtime)/pd.Timedelta('1h'))
         plt.ylabel(ylb[k0])
@@ -207,27 +121,27 @@ def func3():
                 dengoce['lat'], dengoce['long'], source='geo',
                 dest='mlt', datetime=dengoce.index)
     den = [denchamp, dengrace, dengoce]
-    fig2, ax2 = plt.subplots(3, 1, sharex=True)  # Test arglat and orbitn
-    fig6, ax6 = plt.subplots(3, 1, sharex=True)  # Test altitude
+    fig2, ax2 = plt.subplots(2, 1, sharex=True)  # Test arglat and orbitn
+    fig6, ax6 = plt.subplots(2, 1, sharex=True)  # Test altitude
     title = ('CHAMP', 'GRACE', 'GOCE')
     for k0 in range(2): # 0: Champ, 1: Grace, 2: Goce
-        dentmp = den[k0]
-        if dentmp.empty:
+        dent = den[k0]
+        if dent.empty:
             continue
-        dentmp['arglat'] = mf.lat2arglat(dentmp['lat'])
-        dentmp['epoch'] = (dentmp.index - rtime)/pd.Timedelta('1h')
+        dent['arglat'] = mf.lat2arglat(dent['lat'])
+        dent['epoch'] = (dent.index - rtime)/pd.Timedelta('1h')
         # Calculate orbit number. It's better if no data gaps exist.
-        orbitn = np.where(dentmp['arglat'].diff()<0, 1, 0)
+        orbitn = np.where(dent['arglat'].diff()<0, 1, 0)
         orbitn = orbitn.cumsum()
-        dentmp['orbitn'] = orbitn
+        dent['orbitn'] = orbitn
 
         # Test arglat and orbitn
-        ax2[k0].plot(dentmp.epoch, dentmp.orbitn, 'k')
+        ax2[k0].plot(dent.epoch, dent.orbitn, 'k')
         ax2[k0].set_ylabel('Orbit Number (k)')
         ax21 = ax2[k0].twinx()
-        ax21.plot(dentmp.epoch, dentmp.arglat, 'go')
-        ax21.plot(dentmp.epoch, dentmp.lat, 'yo', alpha=0.3)
-        ax21.set_xticks(np.arange(-30, 31, 3))
+        ax21.plot(dent.epoch, dent.arglat, 'go')
+        ax21.plot(dent.epoch, dent.lat, 'yo', alpha=0.3)
+        ax21.set_xticks(np.arange(-30, 31, 1))
         ax21.set_xlim((btime-rtime)/pd.Timedelta('1h'),
                       (etime-rtime)/pd.Timedelta('1h'))
         ax21.set_ylabel('Arglat (g), Lat(y)')
@@ -235,43 +149,50 @@ def func3():
 
         # Test altitude
         height = 'height' if k0<2 else 'alt'
-        ax6[k0].plot(dentmp.epoch, dentmp[height], 'k')
+        ax6[k0].plot(dent.epoch, dent[height], 'k')
         ax6[k0].set_ylabel('Height (km)')
-        ax6[k0].set_xticks(np.arange(-30, 31, 3))
+        ax6[k0].set_xticks(np.arange(-30, 31, 1))
         ax6[k0].set_xlim((btime-rtime)/pd.Timedelta('1h'),
                       (etime-rtime)/pd.Timedelta('1h'))
         ax6[k0].set_title(title[k0])
 
         # Calculate delta density: rho(pass_n)-rho(pass_n-1)
-        dentmp['drho'] = np.nan
-        dentmp['ddmin'] = np.nan
-        for k1 in range(1,dentmp['orbitn'].max()+1):
-            fpp = (dentmp['orbitn'] == k1-1)
-            fpc = (dentmp['orbitn'] == k1)
-            x0lat = dentmp.loc[fpp, 'Mlat'].copy()
-            x0lon = dentmp.loc[fpp, 'MLT'].copy()/12*180
-            x1lat = dentmp.loc[fpc, 'Mlat'].copy()
-            x1lon = dentmp.loc[fpc, 'MLT'].copy()/12*180
+        dent['ptime'] = np.nan
+        dent['ddmin'] = np.nan
+        for k1 in range(1,dent['orbitn'].max()+1):
+            fpp = (dent['orbitn'] == k1-1)
+            fpc = (dent['orbitn'] == k1)
+            x0lat = dent.loc[fpp, 'Mlat'].copy()
+            x0lon = dent.loc[fpp, 'MLT'].copy()/12*180
+            x1lat = dent.loc[fpc, 'Mlat'].copy()
+            x1lon = dent.loc[fpc, 'MLT'].copy()/12*180
             print(k0, x0lat.shape, x1lat.shape)
             dd = np.ones([x1lat.shape[0], x0lat.shape[0]])*np.nan
             for k2 in range(x1lat.shape[0]):
                 dd[k2,:] = mf.great_circle_distance_earth(
                         x0lat, x0lon, x1lat.iloc[k2], x1lon.iloc[k2])
             ddminindex = np.argmin(dd, axis=1)
-            dentmp.loc[fpc, 'ddmin'] = dd[range(dd.shape[0]), ddminindex]
-            prho = np.array(dentmp.loc[fpp, 'msis_rho'].iloc[ddminindex])
-            crho = np.array(dentmp.loc[fpc, 'msis_rho'])
-            dentmp.loc[fpc, 'dmsis_rho'] = 100*(crho-prho)/prho
-            prho = np.array(dentmp.loc[fpp, 'rho400'].iloc[ddminindex])
-            crho = np.array(dentmp.loc[fpc, 'rho400'])
-            dentmp.loc[fpc, 'drho400'] = 100*(crho-prho)/prho
-        den[k0] = den[k0][btime:etime]
+            dent.loc[fpc, 'ddmin'] = dd[range(dd.shape[0]), ddminindex]
+            dent.loc[fpc, 'ptime'] = dent.index[fpp][ddminindex]
+        pmsis = np.array(dent.loc[dent['ptime'], 'msis_rho'])
+        cmsis = np.array(dent['msis_rho'])
+        dent['pmsis_rho'] = pmsis
+        dent['dmsis_rho'] = 100*(cmsis-pmsis)/pmsis
+        prho400 = np.array(dent.loc[dent['ptime'], 'rho400'])
+        crho400 = np.array(dent['rho400'])
+        dent['prho400'] = prho400
+        dent['drho400'] = 100*(crho400-prho400)/prho400
+        prho = np.array(dent.loc[dent['ptime'], 'rho'])
+        crho = np.array(dent['rho'])
+        dent['prho'] = prho
+        dent['drho'] = 100*(crho-prho)/prho
+        #den[k0] = den[k0][btime:etime]
     ax2[-1].set_xlabel('Hours from '+rtime.strftime('%y-%m-%d %H%M UT'))
     ax6[-1].set_xlabel('Hours from '+rtime.strftime('%y-%m-%d %H%M UT'))
 
     # mlat-mlt distribution of satellite orbits
     fig3, ax3 = plt.subplots(
-            3,1,subplot_kw={'projection':'polar'}, figsize=[4.4,7])
+            2,1,subplot_kw={'projection':'polar'}, figsize=[4.4,7])
     cl = list('rbk')
     lb = ['CHAMP', 'GRACE', 'GOCE']
     cls = [ChampDensity, ChampDensity, GoceData]
@@ -279,11 +200,11 @@ def func3():
         fc = 1 if k1 is 'N' else -1
         plt.sca(ax3[k11])
         for k0 in range(3):
-            dentmp = den[k0]
-            if dentmp.empty:
+            dent = den[k0]
+            if dent.empty:
                 continue
-            dentmp.__class__ = cls[k0]
-            hc = dentmp.satellite_position_lt_lat(mag=True, ns=k1)
+            dent.__class__ = cls[k0]
+            hc = dent.satellite_position_lt_lat(mag=True, ns=k1)
             hc.set_color(cl[k0])
             hc.set_label(lb[k0])
         mf.set_lat_lt_polar(plt.gca(), ns=k1, boundinglat=fc*0,
@@ -293,34 +214,34 @@ def func3():
     # Test ddmin
     fig4, ax4 = plt.subplots(2,1,sharex=True)
     for k0 in range(2):
-        dentmp = den[k0].copy()
-        if dentmp.empty:
+        dent = den[k0].copy()
+        if dent.empty:
             continue
-        ax4[k0].plot(dentmp['epoch'], dentmp.ddmin, label=lb[k0], color=cl[k0])
-        ax4[k0].set_xticks(np.arange(-30, 31, 3))
+        ax4[k0].plot(dent['epoch'], dent.ddmin, label=lb[k0], color=cl[k0])
+        ax4[k0].set_xticks(np.arange(-30, 31, 1))
         ax4[k0].set_xlim((btime-rtime)/pd.Timedelta('1h'),
                          (etime-rtime)/pd.Timedelta('1h'))
         ax4[k0].set_title(lb[k0])
         ax4[k0].set_ylabel('$\Delta$r (km)')
     ax4[1].set_xlabel('Hours from '+rtime.strftime('%y-%m-%d %H%M UT'))
 
-    # delta density (rho400 and msis_rho) from reversing time of IMF By
-    fig5, ax5 = plt.subplots(4,2,sharex=True, sharey='row', figsize=(8,9))
+    #  Density difference from orbit to orbit
+    fig5, ax5 = plt.subplots(4,2,sharex=True, figsize=(8,9))
     title = ('CHAMP', 'GRACE')
-    yl = (r'$\rho$ 400 km $(kg/m^{-3})$', r'$\Delta\rho$ (%)',
+    yl = (r'$\rho$ 400 km $(kg/m^{-3})$', r'$\Delta\rho$ 400 km (%)',
           r'Msis $\rho (kg/m^{-3})$', r'Msis $\Delta\rho$ (%)')
     col = ('rho400', 'drho400', 'msis_rho', 'dmsis_rho')
     for k0 in range(2):
-        dentmp = den[k0].copy()
-        if dentmp.empty:
+        dent = den[k0].copy()
+        if dent.empty:
             continue
         for k1 in range(4):
             ax5[k1, k0].plot(
-                    dentmp.epoch, dentmp[col[k1]], label=lb[k0], color=cl[k0])
-            dentmptmp = dentmp[dentmp.Mlat>0]
+                    dent.epoch, dent[col[k1]], label=lb[k0], color=cl[k0])
+            dentmptmp = dent[dent.Mlat>0]
             ax5[k1, k0].plot(
                     dentmptmp.epoch, dentmptmp[col[k1]], 'ko', markersize=3)
-            ax5[k1, k0].set_xticks(np.arange(-30, 31, 3))
+            ax5[k1, k0].set_xticks(np.arange(-30, 31, 1))
             ax5[k1, k0].set_xlim((btime-rtime)/pd.Timedelta('1h'),
                              (etime-rtime)/pd.Timedelta('1h'))
             ax5[0, k0].set_title(title[k0])
@@ -330,6 +251,66 @@ def func3():
             ax5[-1,k0].set_xlabel(
                     'Hours from '+rtime.strftime('%y-%m-%d %H%M UT'))
     plt.tight_layout()
+
+    # Density change as a function of argument of latitude
+    fig7, ax7 = plt.subplots(3,2,sharex=True, figsize=(8,9))
+    title = ('CHAMP', 'GRACE')
+    yl = (r'$\rho$ $(kg/m^{-3})$',
+          r'$\rho$ 400 km $(kg/m^{-3})$', r'Msis $\rho (kg/m^{-3})$')
+    col = ('rho', 'rho400', 'msis_rho')
+    for k0 in range(2):
+        dent = den[k0][btime:etime]
+        if dent.empty:
+            continue
+        for k1 in range(3):
+            for k2 in range(dent['orbitn'].max()+1):
+                dentt = dent[dent.orbitn==k2]
+                ax7[k1, k0].plot(
+                        dentt.arglat, dentt[col[k1]],
+                        label=lb[k0], color=cl[k0])
+                denttt = dentt[dentt.Mlat>0]
+                ax7[k1, k0].plot(
+                        denttt.arglat, denttt[col[k1]], 'ko', markersize=3)
+            ax7[k1, k0].set_xticks(np.arange(0, 361, 60))
+            ax7[k1, k0].set_xlim(0, 360)
+            ax7[0, k0].set_title(title[k0])
+            ax7[k1, 0].set_ylabel(yl[k1])
+            ax7[-1,k0].set_xlabel('Argument of Latitude (degree)')
+    plt.tight_layout()
+
+    # Density change of the current and previous orbits
+    fig8, ax8 = plt.subplots(3,2,sharex=True, figsize=(8,9))
+    title = ('CHAMP', 'GRACE')
+    yl = (r'$\rho$ $(kg/m^{-3})$',
+          r'$\rho$ 400 km $(kg/m^{-3})$', r'Msis $\rho (kg/m^{-3})$')
+    col = (('rho', 'prho'), ('rho400', 'prho400'), ('msis_rho', 'pmsis_rho'))
+    for k0 in range(2):
+        dent = den[k0][btime:etime]
+        if dent.empty:
+            continue
+        for k1 in range(3):
+            ax8[k1, k0].plot(
+                    dent.epoch, dent[col[k1][0]],
+                    label=lb[k0], color=cl[k0])
+            ax8[k1, k0].plot(
+                    dent.epoch, dent[col[k1][1]],
+                    label=lb[k0], color='k', zorder=0)
+            ax8[k1, k0].set_xticks(np.arange(-30, 31, 1))
+            ax8[k1, k0].set_xlim((btime-rtime)/pd.Timedelta('1h'),
+                             (etime-rtime)/pd.Timedelta('1h'))
+            ax8[0, k0].set_title(title[k0])
+            ax8[k1, 0].set_ylabel(yl[k1])
+            ax8[-1,k0].set_xlabel(
+                    'Hours from '+rtime.strftime('%y-%m-%d %H%M UT'))
+    plt.tight_layout()
+    fig1.savefig('/home/guod/Documents/work/fig/w03_func03_02a')
+    fig2.savefig('/home/guod/Documents/work/fig/w03_func03_02b')
+    fig3.savefig('/home/guod/Documents/work/fig/w03_func03_02c')
+    fig4.savefig('/home/guod/Documents/work/fig/w03_func03_02d')
+    fig5.savefig('/home/guod/Documents/work/fig/w03_func03_02e')
+    fig6.savefig('/home/guod/Documents/work/fig/w03_func03_02f')
+    fig7.savefig('/home/guod/Documents/work/fig/w03_func03_02g')
+    fig8.savefig('/home/guod/Documents/work/fig/w03_func03_02h')
     return
 # END
 #------------------------------------------------------------
